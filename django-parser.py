c46de5854f20db1577a576ab7ae01176ee2b9574
Junwen Yang <junwenyang@Junwens-MacBook-Pro.local>
2020-06-21 22:54:25 -0500

    add nodevisitor

---
+++ django-parser.py
@@ -0,0 +1,25 @@
+import ast
+class VisitCall(ast.NodeVisitor):
+
+    def visit_Call(self, node):
+        functions = ['CreateModel', 'DeleteModel', 'RenameModel', 'AddField', 'RemoveField', 'AlterField', 'RenameField']
+        func = node.func
+        # Call(expr func, expr* args, keyword* keywords)
+        if type(func) == ast.Attribute:
+            # Attribute(expr value, identifier attr, expr_context ctx) attr is used for function name
+            funcname = func.attr
+            if funcname in functions:
+                #args and keywords are used for args. keywords are more like hash 
+                args = node.keywords
+                print("function name is {}".format(funcname))
+                for arg in args:
+                    v = arg.value
+                    if(type(arg.value) == ast.Str):
+                        v = arg.value.s
+                    print("{} {}".format(arg.arg, v))
+file = "test.py"
+contents = open(file).read()
+tree = ast.parse(contents)
+
+vc = VisitCall()
+vc.visit(tree)
